555b4b9f672a16f24eacc73f89a2bb9eacefeeb9
Sodi <tassaduq009@gmail.com>
2026-08-05 15:19:03 +0500

    Yes, it is fixed and deployed. Live build: 239c9a27b33407e6b735 Frontend now uses https://api.battlecities.com Google authentication returns 302 to accounts.google.com Production builds now default to the Oracle API in [prod.config.js (line 3)](C:/repos/BattleCity/webpack/prod.config.js:3) Hard-refresh the existing browser tab once to replace the cached JavaScript.

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,5 +1,7 @@
 const { merge } = require('webpack-merge');
 
+process.env.BATTLECITY_API_BASE_URL ||= 'https://api.battlecities.com';
+
 const baseConfig = require('./base.config');
 
 module.exports = merge(baseConfig, {
