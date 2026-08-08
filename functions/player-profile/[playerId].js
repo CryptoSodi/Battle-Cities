7aed5000fa312fe8b10f084d826df879d8b4f932
Sodi <tassaduq009@gmail.com>
2026-08-09 00:12:16 +0500

    Fix Cloudflare profile rewrite

--- functions/player-profile/[playerId].js
+++
@@ -1,5 +0,0 @@
-export function onRequestGet(context) {
-  const profileUrl = new URL(context.request.url);
-  profileUrl.pathname = "/player-profile/";
-  return context.env.ASSETS.fetch(profileUrl);
-}
